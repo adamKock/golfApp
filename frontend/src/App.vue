4fd81a5b5a12879fbad2e8e658c90f344101d30c
Adam Kockelbergh <akock001@campus.goldsmiths.ac.uk>
2025-08-08 16:13:46 +0100

    Commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,10 @@
 import AddRound from './components/AddRound.vue'
 import HomePage from './components/HomePage.vue'
 import ChartComponent from './components/PieChartComponent.vue';
+import ChartComponent from './components/BarChartComponent.vue';
+import ChartComponent from './components/LineChartComponent.vue';
+
+
 
 export default {
   name: 'App',
